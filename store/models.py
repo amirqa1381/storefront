12a09c8fcb9aee33f0ec2dc278d2921bd77246ef
Amir Hussein <amirghasemian1381@gmail.com>
2025-01-17 19:18:46 +0330

    modified the models and make the migration for them

--- store/models.py
+++ store/models.py
@@ -9,16 +9,19 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+    slug = models.SlugField()
     
     
 class Customer(models.Model):
@@ -66,7 +69,7 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    zip = models.CharField(max_length=10)
+    zip_code = models.CharField(max_length=10)
     
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
